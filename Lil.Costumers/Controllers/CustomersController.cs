3de50bec6ceeda7558049da8f283c922c1b389ee
Franklin <franklin.pc7@gmail.com>
2024-02-14 23:31:24 -0600

    Construct the Lil.Customers Proyect, add class and controller

---
+++ Lil.Costumers/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using Lil.Costumers.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lil.Costumers.Controllers
+{
+	[Route("api/[controller]")]
+	public class CustomersController:ControllerBase
+	{
+		private readonly ICustomersProvider _customersProvider;
+
+		public CustomersController(ICustomersProvider customersProvider)
+		{
+			this._customersProvider = customersProvider;
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetAsync(string id)
+		{
+			var customer = await this._customersProvider.GetAsync(id);
+
+			if (customer != null)
+				return Ok(customer);
+
+			return NotFound();
+		}
+
+	}
+}
---
+++ Lil.Costumers/DAL/ICustomersProvider.cs
@@ -0,0 +1,13 @@
+﻿using Lil.Costumers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lil.Costumers.DAL
+{
+	public interface ICustomersProvider
+	{
+		Task<Customer> GetAsync(string id);
+	}
+}
