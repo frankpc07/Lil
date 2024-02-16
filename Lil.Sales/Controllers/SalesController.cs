bf16312c313ddd78a0b0f4f6c55618e124d599ec
Franklin <franklin.pc7@gmail.com>
2024-02-15 23:44:53 -0600

    Create api Sales and models and interface

---
+++ Lil.Sales/Controllers/SalesController.cs
@@ -0,0 +1,32 @@
+﻿using Lil.Sales.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lil.Sales.Controllers
+{
+	[Route("api/[controller]")]
+	
+	public class SalesController : ControllerBase
+	{
+		private readonly ISalesProvider _salesProvider;
+
+		public SalesController(ISalesProvider salesProvider)
+		{
+			this._salesProvider = salesProvider;
+		}
+
+		[HttpGet("{customerId}")]
+		public async Task<IActionResult> GetAsync(string customerId)
+		{
+			var orders = await this._salesProvider.GetAsync(customerId);
+
+			if (orders != null && orders.Any())
+				return Ok(orders);
+
+			return NotFound();
+		}
+	}
+}
